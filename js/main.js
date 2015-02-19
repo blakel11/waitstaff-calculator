b07068f55eea09de81f4a51e5c15cc72d67a7328
Blake <blake.lieberman@gmail.com>
2015-02-19 12:30:49 -0800

    add functionality to submit, calculator, earnings and charges

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
 angular.module('myApp',['ngMessages'])
 	.controller('MyCtrl', ['$scope', function($scope){
-		
+		$scope.metrics = {
+			subTotal : null,
+			tip : null,
+			tipTotal : null,
+			mealCount : null
+		};
+
+		$scope.calc = {};
+
+		$scope.submit = function(){
+			if($scope.detailsForm.$valid) {
+				console.log('This is valid');
+				$scope.metrics.subTotal = $scope.calc.basePrice * (1 + ($scope.calc.taxRate/100));
+				$scope.metrics.tip = $scope.calc.basePrice * ($scope.calc.tip/100);
+
+				$scope.metrics.tipTotal += $scope.calc.basePrice * ($scope.calc.tip/100);
+				$scope.metrics.mealCount ++;
+
+				$scope.calc = {};
+			} else {
+				console.log('This is invalid');
+			}
+		};
+		$scope.cancel = function(){
+			console.log('cancel');
+			$scope.calc = {};
+		};
+		$scope.reset = function(){
+
+		};
 	}]);
